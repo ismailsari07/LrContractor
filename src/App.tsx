0d3704d41099310eb96eccc5b670c53e6348027b
ismailsari07 <xismail.sari@gmail.com>
2025-04-26 16:26:09 -0400

    chore: set up initial project folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+
 function App() {
   return (
     <div className="App">
-      <div className="min-h-screen bg-neutral-100 flex flex-col items-center justify-center text-neutral-900 font-sans font-bold">
-        <h1 className="text-4xl font-heading text-primary-700">Tailwind Çalışıyor 🎉</h1>
-      </div>
     </div>
   );
 }
---
+++ src/components/Tab.tsx
@@ -0,0 +1,5 @@
+const Tab = function () {
+  return <div></div>;
+};
+
+export default Tab;
